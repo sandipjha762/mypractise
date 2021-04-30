91382a84e8eee85fc818e4d2c006d88852a341d8
sandipjha762 <sandipjha762@gmail.com>
2021-04-30 15:26:53 +0530

    add

---
+++ basic/app.js
@@ -0,0 +1,8 @@
+
+export function app(num){
+    console.log("I am from app class "+num);
+}
+
+export const add=(n1,n2)=>console.log(n1+n2);
+export const sub=(n1,n2)=>console.log(n1-n2);
+
--- basic/script.js
+++ basic/script.js
@@ -1,9 +1,6 @@
-function init(){
-    document.getElementById("hey").style.display="none";
-}
-
-function btn_click(){
-    console.log("hey");
-    console.log("hello");
-    document.getElementById('hey').style.display='block';
-}
+import {add, sub} from './app.js';
+const hello=[1,2,3,4];
+let [a,b,c]=hello;
+add(15,25);
+sub(20,5);
+console.log(a , b,c);
