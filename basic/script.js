34fd4873c2fad272c7a8a3b4146107f8bd7f1207
sandipjha762 <sandipjha762@gmail.com>
2021-04-30 14:49:35 +0530

    basic

---
+++ basic/script.js
@@ -0,0 +1,8 @@
+function init(){
+    document.getElementById("hey").style.display="none";
+}
+
+function btn_click(){
+    console.log("hey");
+    document.getElementById('hey').style.display='block';
+}
